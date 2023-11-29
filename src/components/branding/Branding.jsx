03112d603fd20cad65f77ebe4a170d2608926508
SAGAR SAHU <sagarsahurock9@gmail.com>
2023-11-30 01:31:35 +0530

    Portfolio commit 1

---
+++ src/components/branding/Branding.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Branding = () => {
+  return (
+    <div>
+    <div >
+    <h1>Key Benefits of Having a Good Web Design</h1>
+    <h2>An Impactful First Impression</h2>
+    <p>
+    With a well-designed website, it becomes easier to make a grand first impression. For brands, it is all about how potential customers or clients view their business, and this is greatly influenced by how they view their website. Your website represents your brand, and when you focus on web designing, it can help you create a compelling and powerful first impression.
+    </p>
+    <h2>Better Google Rankings</h2>
+    <p>
+    Web designing has a variety of elements that are capable of bestowing your website with better Google rankings. Some of these elements include readability, mobile-friendliness, website speed, sitemaps, URL structure, and website navigation. A good web design has these elements incorporated into the website, which gives you greater visibility both for your audience and the search engine.
+    </p>
+    <h2>Brand Consistency </h2>
+    <p>
+    Several key assets of your brand, such as the logo, color scheme, and fonts can ensure your audience remembers your brand and easily differentiates it from thousands of others. A professional web designer takes into account all the unique aspects of your business and blends them into your website to add to your brand credibility and portray consistency.
+    </p>
+    <h2>Minimized Bounce Rates</h2>
+    <p>When your website is immersive and interesting, it encourages visitors to go deeper and explore different sections of the website. Likewise, a website that is seemingly complicated and difficult to navigate can prompt visitors to exit it within seconds. Bounce rates also impact the credibility and rankings of your website. When your website is designed to retain visitors for longer, you can escape premature exits and minimize bounce rates.</p>
+    </div>
+      <div>
+        <h1>Key Elements of Web Design</h1>
+        <p>A good website is one that displays a careful balance between appearance and functionality. When your website is aesthetically pleasing but fails to offer the information or help your audience with what it looks for, the aesthetics cannot make up for the lack of functionality and vice versa. Therefore, web design comprises two broad key elements—aesthetic or visual elements and functional elements—that are of equal importance to make your website stand out.</p>
+      <h3>Visual Elements</h3>
+      <p>Visual elements come together and set a theme for your website, which leaves a great impact on visitors when you get it right. It is crucial that every element compliments one another and looks harmonious rather than chaotic. Visual elements include:</p>
+      <ul>
+        <li>Fonts</li>
+        <li>Colors</li>
+        <li>Shapes</li>
+        <li>Layout</li>
+        <li>Icons</li>
+        <li>Logos</li>
+        <li>Images</li>
+      </ul>
+      </div>
+      <div>
+        <h2>What is Responsive Web Design?</h2>
+        <p>Responsive web design is a technique used to make certain changes to a website such that it adapts to the screen size and orientation of different devices. A website designed for bigger screens does not fit small screens seamlessly, which causes inconvenience for viewers. Therefore, responsive web design has become a very important factor that influences user experience in recent times, as people use their smartphones and tablets to browse through websites.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Branding
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import "./services.scss";
 import {useInView, motion} from "framer-motion";
+import Branding from "../branding/Branding";
 
 const variants={
     initial:{
@@ -42,32 +43,30 @@ const Services = () => {
         <div className="title">
         
         <h1><motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business.</h1>
-        <motion.button  whileHover={{ scale: 1.2 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 17 }}>WHAT WE DO?</motion.button>
+        <motion.button>WHAT WE DO?</motion.button>
 
         </div>
     </motion.div>
     <motion.div className="listContainer">
         <motion.div className="box" whileHover={{ scale: 1.1,background:"lightgray",color:"black" }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-        <h2>Branding</h2>
-        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        <button>Go</button>
+        <h2>UI/UX Design</h2>
+        <p>A good web design can transform how your brand speaks to your audience. We focus on your web design more than ever and look for professional and smooth design .</p>
+        <button> <a href="../branding/Branding">Go</a></button>
         </motion.div>
         <motion.div className="box" whileHover={{ scale: 1.1,background:"lightgray",color:"black" }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-        <h2>Branding</h2>
-        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        <button>Go</button>
+        <h2>Web & Mobile Application</h2>
+        <p>A good  application combines great design with functionality, and business needs with long-term objectives—improving business processes and building better customer relationships.</p>
+        <button><a href="https://powerapps.microsoft.com/en-us/what-makes-a-good-app/">Go</a></button>
         </motion.div>
         <motion.div className="box" whileHover={{ scale: 1.1,background:"lightgray",color:"black" }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-        <h2>Branding</h2>
-        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        <button>Go</button>
+        <h2>Digital Marketing</h2>
+        <p>digital marketing encompasses everything from web design, search engine optimization (SEO), pay-per-click (PPC) advertising, content creation, and management and social media management.</p>
+        <button><a href="https://www.8ways.ch/en/digital-news/web-design-and-digital-marketing-good-combination#:~:text=At%20its%20core%2C%20digital%20marketing,through%20websites%20and%20landing%20pages.">Go</a></button>
         </motion.div>
         <motion.div className="box" whileHover={{ scale: 1.1,background:"lightgray",color:"black" }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-        <h2>Branding</h2>
-        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
-        <button>Go</button>
+        <h2>Ecommerce Solution</h2>
+        <p>The solutions refer to a set of software and technology tools that help businesses sell products and services online. include a shopping cart, payment processing systems, and inventory management tools.</p>
+        <button><a href="https://www.hurix.com/everything-you-need-to-know-about-e-commerce-solutions/#:~:text=E%2Dcommerce%20solutions%20refer%20to,systems%2C%20and%20inventory%20management%20tools.">Go</a></button>
         </motion.div>
     </motion.div>
       
